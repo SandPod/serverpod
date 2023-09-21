0e8102820e41d7696890648e8aa4a3c6b49a8a1c
Alexander Sandor <alexander@serverpod.dev>
2023-09-21 11:42:39 +0200

    generate: generate all projects

--- modules/serverpod_auth/serverpod_auth_client/lib/src/protocol/google_refresh_token.dart
+++ modules/serverpod_auth/serverpod_auth_client/lib/src/protocol/google_refresh_token.dart
@@ -4,6 +4,7 @@
 // ignore_for_file: library_private_types_in_public_api
 // ignore_for_file: public_member_api_docs
 // ignore_for_file: implementation_imports
+// ignore_for_file: non_constant_identifier_names
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
