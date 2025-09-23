c0558ceb1f73fd626ae543f99002eb213f19eb0c
Alexander Sandor <alexander@serverpod.dev>
2025-09-23 16:14:43 +0200

    chore: initialize benchmark test repo

---
+++ examples/benchmark/benchmark_test/bin/benchmark_test.dart
@@ -0,0 +1,5 @@
+import 'package:benchmark_test/benchmark_test.dart' as benchmark_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${benchmark_test.calculate()}!');
+}
---
+++ examples/benchmark/benchmark_test/test/benchmark_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:benchmark_test/benchmark_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
